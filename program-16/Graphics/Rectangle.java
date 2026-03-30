9fefb719b9d133dc80660794e8fef29ff5fb4853
KAVYAMOHAN1234 <kavyamohan253@gmail.com>
2026-03-30 15:54:39 +0530

    Add files via upload

---
+++ program-16/Graphics/Rectangle.java
@@ -0,0 +1,15 @@
+package Graphics;
+public class Rectangle implements GraphicsRect {
+int length;
+int breadth;
+public Rectangle(int l, int b) {
+length = l;
+breadth = b;
+}
+public int perimeter() {
+return 2 * (length + breadth);
+}
+public int area() {
+return length * breadth;
+}
+}
